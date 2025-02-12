fa2117bec746ce2b32e06c1ae567421efa2d691b
PaNDa2code <moaaz0688@gmail.com>
2025-02-12 18:59:09 +0200

    feat: implement UpdateRingBuffer

--- include/ring_buffer.h
+++ include/ring_buffer.h
@@ -2,20 +2,14 @@
 
 #include <windows.h>
 
-typedef struct _rw
-{
-} RW, *PRW;
-
 typedef struct _ring_buffer
 {
-  SIZE_T start;
-  SIZE_T end;
   PVOID BaseBuffer;
   SIZE_T BufferSize;
-  LPVOID pSecondaryView;
-  PRW rw;
+  SIZE_T ReadOffset;
+  SIZE_T WriteOffset;
 } RING_BUFFER, *PRING_BUFFER;
 
 BOOL CreateRingBuffer(PRING_BUFFER pRingBuffer, SIZE_T Size);
 void FreeRingBuffer(PRING_BUFFER pRingBuffer);
-BOOL UpdateBuffer(PRING_BUFFER pRingBuffer, HANDLE hRead);
+BOOL UpdateRingBuffer(PRING_BUFFER pRingBuffer, HANDLE hRead);
