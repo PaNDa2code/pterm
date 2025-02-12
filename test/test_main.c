86bfff5486793ed7c7a3a8e112b8e73348c0ae95
PaNDa2code <moaaz0688@gmail.com>
2025-02-12 18:58:28 +0200

    test: add ring buffer creation test

--- test/test_main.c
+++ test/test_main.c
@@ -2,8 +2,19 @@
 
 #include "ring_buffer.h"
 
-MunitResult test_ring_buffer_create(const MunitParameter params[], void *user_data_or_fixture) {
-  Sleep(10000);
+MunitResult test_ring_buffer_create(const MunitParameter params[], void *user_data_or_fixture)
+{
+  const unsigned long size = 64 KB;
+  RING_BUFFER ring_buffer = {};
+  unsigned char *buffer = malloc(size);
+  if (buffer == NULL)
+    return MUNIT_SKIP;
+  munit_assert_true(CreateRingBuffer(&ring_buffer, size));
+  munit_rand_memory(size, buffer);
+  memcpy(ring_buffer.BaseBuffer + size / 2, buffer, size);
+  munit_assert_memory_equal(size, ring_buffer.BaseBuffer + size / 2, buffer);
+  FreeRingBuffer(&ring_buffer);
+  free(buffer);
   return MUNIT_OK;
 };
 
@@ -12,13 +23,7 @@ MunitTest tests[] = {
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
 };
 
-static const MunitSuite suite = {
-    "/pterm-tests",
-    tests,
-    NULL,
-    1,
-    MUNIT_SUITE_OPTION_NONE
-};
+static const MunitSuite suite = {"/pterm-tests", tests, NULL, 1, MUNIT_SUITE_OPTION_NONE};
 
 int main(int argc, char *const *argv)
 {
